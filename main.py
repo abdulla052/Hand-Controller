8d4c307fbcf048eec35f74ed456ff74695af4355
AbDullA <105763552+abdulla052@users.noreply.github.com>
2023-06-02 16:52:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import CarGameController as CarGame
+import pointerGameController as Pointer
+import leftHolderMouseButton as Spliter
+
+
+# CarGame.game() # 2 handtracker
+# Pointer.game() # 2 finger for click and 1 finger for changing position
+Spliter.game()  # 5 finger to unpress and 1 finger to start pressing
