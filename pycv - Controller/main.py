fc09404fe5265e6225d64f43b419bbebfbc10d40
AbDullA <abdullahassan052@gmail.com>
2024-04-04 15:44:14 +0300

    Update main.py

--- pycv - Controller/main.py
+++ pycv - Controller/main.py
@@ -2,7 +2,7 @@ import CarGameController as CarGame
 import pointerGameController as Pointer
 import leftHolderMouseButton as Spliter
 
-
-# CarGame.game() # 2 handtracker
+CarGame.game() # 2 handtracker
+# Spliter.game()  # 5 finger to unpress and 1 finger to start pressing
 # Pointer.game() # 2 finger for click and 1 finger for changing position
-Spliter.game()  # 5 finger to unpress and 1 finger to start pressing
+    
